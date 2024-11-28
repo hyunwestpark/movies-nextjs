3371adf17edef345c3a3ea9a2a24d46aecd06a66
hyunwestpark <rainday0828@gmail.com>
2024-11-28 12:16:30 +0900

    add styles

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,18 +1,24 @@
 import { Suspense } from "react";
 import { API_URL } from "../../../(home)/page";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
+
+export async function generateMetadata({ params }) {
+  const { id } = await params;
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
 
 export default async function MovieDetail({ params, searchParams }) {
   const { id } = await params;
 
   return (
     <div>
-      <h3>Movie Detail Page</h3>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
-      <h4>Videos</h4>
       <Suspense fallback={<h1>Loading movie video</h1>}>
         <MovieVideos id={id} />
       </Suspense>
